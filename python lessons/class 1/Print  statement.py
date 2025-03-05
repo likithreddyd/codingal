9c59a708e0c706eb2f1113687b62daaf4782c1b3
likith <likithreddy2011@gmail.com>
2025-03-05 08:25:30 +0530

    print statement

---
+++ python lessons/class 1/Print  statement.py
@@ -0,0 +1,7 @@
+print("I am learning python")
+print(23+45)
+print(29*36)
+print(29/3)
+print("reminder is ", 25%5)
+print(34 ** 45, "is the value")
+
--- python lessons/class 1/intro.py
+++ python lessons/class 1/intro.py
@@ -1,10 +1,21 @@
 print("hello")
 print('lIKITH')
 
-#  NOTE :KDJFDM
+#  NOTE :Evwery thing should come in one line
 
 
 # git add .
 # git commit -m "we have learnt print and comment"
 # git push
 
+print("Hi how are you")
+
+print("Hello I am fine how are you")
+
+print("I am fine too")
+# NOTE Choose your answer
+print("Who is the CM of andra pradesh")
+
+1. Modi
+2. Likith
+3. FAN
