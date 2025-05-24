f42433d95149f3d51616db1cc6a04dd1d28b1eb8
likith <likithreddy2011@gmail.com>
2025-05-24 20:21:34 +0530

    hi

---
+++ python lessons/class 27/Abstract Meathod.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+class Tesla(ABC):
+    def runsOnFuel(self):
+        print("This car runs on fuel")
+    def runsOnBattries(self):
+        print("This car runs on Battries")
+    
+    def autoDrive(self):
+        pass
+
+class Model1(Tesla):
+    def autoDrive(self):
+        print("This car can autodrive or drive on itself without a driver")
+    
+car1 = Model1()
+car1.autoDrive()
+car1.runsOnBattries()
+car1.runsOnFuel()
